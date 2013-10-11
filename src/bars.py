19d3374a762329da37272116286e6e4a989b76b3
Shashi Gowda <shashigowda91@gmail.com>
2013-10-12 01:37:57 +0530

    Added comments to bars example

--- src/bars.py
+++ src/bars.py
@@ -1,3 +1,4 @@
+# An example showing how to use the audio package
 import pygame
 from pygame import display
 from pygame.draw import *
@@ -5,12 +6,19 @@ from pygame.draw import *
 import audio
 from signalutil import *
 
-# Open a pygame display
+# Open a pygame display, required for showing the rectangles
 pygame.init()
 surface = display.set_mode((640, 480))
+
+# read audio data into a numpy array, sF is the sampling frequency
 sF, data = audio.read("/home/shashi/death.mp3")
+# soundObj is a pygame.mixer.Sound object
 soundObj = audio.makeSound(sF, data)
 
+# this function is passed to audio.playAndRun to be run as
+# often as required and possible.
+# i is approximately the sample number currently being played
+# delta is the number of samples since the previous call to loop
 def loop(i, delta):
     if delta > 0:
         fps = sF / delta
@@ -21,4 +29,5 @@ def loop(i, delta):
         barGraph(surface, (20, 40, 600, 400), bins)
         display.update()
 
+# pass the Sound object and loop function, set update frequency to 90Hz
 audio.playAndRun(soundObj, loop, 90)
